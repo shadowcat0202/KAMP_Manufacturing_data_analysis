99908f13027cf13cda71f2585c17e27c4695ae14
Sean Lee <ai.engr.sy@gmail.com>
2022-10-26 21:22:56 +0900

    데이터전처리-10.26.22 (초(sec) 추가 / STD, NUM 삭제 /  TAG > OK?)

--- DataVisualization.py
+++ DataVisualization.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import missingno as msno
 
+
+
 from autoviz.AutoViz_Class import AutoViz_Class
 
 class DataVisualization():
     def __init__(self):
         pass
-
+    #
+    # def visualize_asMuch(self):
     # av = AutoViz_Class()
     # av.AutoViz(filename= '',
     #            dfte = dt.df_org,
@@ -18,6 +21,6 @@ class DataVisualization():
     #            verbose = 2,
     #            max_rows_analyzed= dt.df_org.shape[0],
     #            max_cols_analyzed= dt.df_org.shape[1])
-
-
+    #
+    #
 
